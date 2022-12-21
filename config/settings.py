d6977ad51a86d699006ac079766be415d2c71060
voonkvuno <voonkvuno@naver.com>
2022-12-21 13:03:37 +0900

    Freeze requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -7,10 +7,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from datetime import timedelta
 import json
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'users',
     'subscriptions',
     'alarms',
+    'django_extensions',
 
     # DRF
     "rest_framework",
