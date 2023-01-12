7f53bddd2de90e79e66f098e5b4c8f7adb5df856
25th-Night <brown_eyed87@naver.com>
2023-01-12 22:27:20 +0900

    Add subscriptionhistory func

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -77,3 +77,49 @@ class SubscriptionDetail(APIView):
             subscription_serializer.save()
             return Response(subscription_serializer.data, status=status.HTTP_200_OK)
         return Response(subscription_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SubscriptionHistory(APIView):
+    permission_classes = [IsOwnerOnly]
+    authentication_classes = [JWTAuthentication]
+    serializer_class = SubscriptionSerializer
+    pagination_class = CustomPagination
+
+    @property
+    def paginator(self):
+        if not hasattr(self, '_paginator'):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                self._paginator = self.pagination_class()
+        return self._paginator
+
+    def paginate_queryset(self, queryset):
+        if self.paginator is None:
+            return None
+        return self.paginator.paginate_queryset(queryset, self.request, view=self)
+
+    def get_paginated_response(self, data):
+        assert self.paginator is not None
+        return self.paginator.get_paginated_response(data)
+
+    def get(self, request):
+        sub_list = Subscription.objects.select_related('user', 'plan', 'billing', 'alarm_subscription').filter(user=request.user, delete_on=0)
+        sub_now = list(sub_list.filter(is_active=1))
+        sub_now.sort(key=lambda x: x.next_billing_at(), reverse=True)
+        sub_exp = list(sub_list.filter(is_active=0).order_by("-expire_at"))
+        subscription_list = sub_now + sub_exp
+        page = self.paginate_queryset(subscription_list)
+        if page is not None:
+            serializered_subscription_data = self.serializer_class(page, many=True).data
+            return self.get_paginated_response(serializered_subscription_data.data)
+
+        serializered_subscription_data = self.serializer_class(subscription_list, many=True).data
+        return Response(serializered_subscription_data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        list_selected = request.data['list_selected']
+        
+        subscription_list = Subscription.objects.filter(user = request.user, delete_on=False)
+        target_subscription = subscription_list.filter(id__in=list_selected)
+        target_subscription.update(delete_on=1)
+        return Response({"message": "정상"}, status=status.HTTP_200_OK)
