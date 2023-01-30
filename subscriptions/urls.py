e95e561dc3e91b850bb745292c4f93de1e83913b
25th-Night <brown_eyed87@naver.com>
2023-01-30 15:32:59 +0900

    Update PlanListView url path

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from subscriptions.views import SubscriptionList, SubscriptionDetail, SubscriptionHistory, CategoryListView, ServiceListView, plan_data, price_data, type_data, company_data, dday_data
+from subscriptions.views import SubscriptionList, SubscriptionDetail, SubscriptionHistory, CategoryListView, ServiceListView, PlanListView, price_data, type_data, company_data, dday_data
 
 urlpatterns = [
     path("main/", SubscriptionList.as_view(), name="subscription_list"),
@@ -7,7 +7,7 @@ urlpatterns = [
     path("history/", SubscriptionHistory.as_view(), name="subscription_history"),
     path("category/", CategoryListView.as_view(), name="category_list"),
     path("service/", ServiceListView.as_view(), name="service_list"),
-    path("plan/", plan_data, name="plan_list"),
+    path("plan/", PlanListView.as_view(), name="plan_list"),
     path("price/", price_data, name="price_list"),
     path("method_type/", type_data, name="method_type_list"),
     path("company/", company_data, name="company_list"),
